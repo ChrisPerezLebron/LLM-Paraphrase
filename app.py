deaad73b7aea16e71e84abea49aeaa6d245167cf
ChrisPerezLebron <98716656+ChrisPerezLebron@users.noreply.github.com>
2025-09-30 09:01:42 -0400

    Add endpoint to process input text
    Endpoint is incomplete, just verifying the connection

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def main_page():
     return render_template("index.html")
+
+@app.route("/api/paraphrase", methods=['POST'])
+def paraphrase():
+    data = request.get_json()
+
+    inputText = data.get("text")
+    print(type(inputText))
+    print(inputText)
+
+    return jsonify({"output": "none"}), 200
+
