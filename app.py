44cd168d4e338a13751360be1d01e6e83d155cd9
ChrisPerezLebron <98716656+ChrisPerezLebron@users.noreply.github.com>
2025-09-30 09:48:17 -0400

    Added backend code to generate rephrased responses
    Using OLlama to generate responses for the frontend. Current version
    avoids streaming. I will consider streaming later down the line.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, render_template, request
+import ollama
+import re
 
 app = Flask(__name__)
+ollama.pull('llama3.2')
 
 @app.route("/")
 def main_page():
@@ -13,6 +16,49 @@ def paraphrase():
     inputText = data.get("text")
     print(type(inputText))
     print(inputText)
+    
+    professionalPrompt = f''' 
+        Rewrite the following text in a professional tone. Just respond with the professional version don't include any 
+        other explanation or preamble text:
 
-    return jsonify({"output": "none"}), 200
+        '{inputText}'
+    '''
+    professionalResponse =  ollama.generate(model="llama3.2", prompt=professionalPrompt)
+
+
+    politePrompt = f''' 
+        Rewrite the following text in a polite tone. Just respond with the polite version don't include any 
+        other explanation or preamble text:
+
+        '{inputText}'
+    '''
+    politeResponse =  ollama.generate(model="llama3.2", prompt=politePrompt)
+
+
+    casualPrompt = f''' 
+        Rewrite the following text in a casual tone. Just respond with the casual version don't include any 
+        other explanation or preamble text:
+
+        '{inputText}'
+    '''
+    casualResponse =  ollama.generate(model="llama3.2", prompt=casualPrompt)
+
+
+    socialMediaPrompt = f''' 
+        Rewrite the following text in a social media tone. That is, in a tone suitable for social media posts and ultra 
+        casual conversations. Remember, text on social media tends to use lots of emojis and slang. Just respond with 
+        the social media version don't include any other explanation or preamble text:
+
+        '{inputText}'
+    '''
+    socialMediaResponse =  ollama.generate(model="llama3.2", prompt=socialMediaPrompt)
+
+
+    
+
+    return jsonify({"professional": professionalResponse['response'],
+                    "casual": casualResponse['response'],
+                    "polite": politeResponse['response'],
+                    "social-media": socialMediaResponse['response']
+                    }), 200
 
